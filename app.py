00d274fab0b27ca1413a8d37dbdf22dd7baae3d2
Evgeny Eroshin <eee333@yandex.ru>
2021-10-22 16:00:07 +0300

    Add func for search by release_year

--- app.py
+++ app.py
@@ -22,12 +22,28 @@ app = Flask(__name__)
 
 
 @app.route('/movie')
-def search_title():
-    # Example: http://127.0.0.1:5000/movie?title=Tarzan
-    title = request.args.get('title')
-    result = query_db(f"SELECT title, country, release_year, listed_in, description FROM netflix WHERE title = '{title}' ORDER BY release_year DESC")
-    body = json.dumps(result)
+def search():
+    result = []
     status = '200'
+    title = request.args.get('title') # Example: http://127.0.0.1:5000/movie?title=Tarzan
+    year = request.args.get('year') # Example: /movie?year=1973-1975 or /movie?year=1975
+    if title:
+        result = query_db(f"SELECT title, country, release_year, listed_in, description FROM netflix WHERE title = '{title}' ORDER BY release_year DESC")
+    elif year:
+        if '-' in year:
+            years = year.split('-')
+            years[0] = int(years[0])
+            years[1] = int(years[1])
+            result = query_db(
+                f"SELECT title, release_year FROM netflix WHERE release_year BETWEEN {years[0]} AND {years[1]} ORDER BY release_year DESC LIMIT 100")
+        else:
+            year = int(year)
+            result = query_db(
+                f"SELECT title, release_year FROM netflix WHERE release_year = {year}  ORDER BY release_year DESC LIMIT 100")
+    if not result:
+        result = {"error": "Not found"}
+        status = '404'
+    body = json.dumps(result)
     response = Response(body, content_type='application/json', status=status)
     return response
 
